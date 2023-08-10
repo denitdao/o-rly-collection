cfb63aec3311ca8c65077c1f5cce77f1dbba9c82
Denys Churchyn <dodenisa@gmail.com>
2023-08-10 11:46:54 +0300

    Popup breathing effect

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -60,15 +60,18 @@ const Popup: React.FC<PopupState> = (props) => {
             transition: "all 0.5s",
           }}
         >
-          <svg
-            className="mr-3 inline h-4 w-4 flex-shrink-0"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-          </svg>
+          <span className="relative mr-3 flex h-4 w-4">
+            <span className="absolute h-full w-full animate-ping rounded-full bg-green-500 opacity-75"></span>
+            <svg
+              className="absolute flex-shrink-0"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+          </span>
           <span className="font-medium">{props.message}</span>
         </div>
       )}
