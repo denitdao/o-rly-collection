6c893123555074d10cc4768f879d895becf83cec
Denys Churchyn <dodenisa@gmail.com>
2023-08-14 20:21:08 +0300

    Updated OG

--- src/components/OrlyHead.tsx
+++ src/components/OrlyHead.tsx
@@ -1,14 +1,41 @@
 import Head from "next/head";
+import { env } from "~/env.mjs";
 
-const OrlyHead = () => {
+type HeadProps = {
+  title?: string;
+  description?: string;
+  imageUrl?: string;
+};
+
+/**
+ * Smart head component for the page to update meta tags.
+ * @param title title of the page
+ * @param description description of the page
+ * @param imageUrl page cover image
+ */
+const OrlyHead = ({
+  title = "O'RLY Covers",
+  description = "Find O'RLY meme book cover. Use for your programming arguments.",
+  imageUrl = `${env.NEXT_PUBLIC_OG_SOURCE}/og.jpeg`,
+}: HeadProps) => {
   return (
     <Head>
-      <title>Meme Book Search</title>
-      <meta
-        name="description"
-        content="Search and explore meme books about programming"
-      />
+      <title>{title}</title>
+      <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content={imageUrl} />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:site_name" content="O'RLY Covers" />
+      <meta property="og:determiner" content="the" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
     </Head>
   );
 };
--- src/env.mjs
+++ src/env.mjs
@@ -18,6 +18,7 @@ export const env = createEnv({
    */
   client: {
     NEXT_PUBLIC_IMAGE_SOURCE: z.string().min(1),
+    NEXT_PUBLIC_OG_SOURCE: z.string().min(1),
   },
 
   /**
@@ -28,6 +29,7 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_IMAGE_SOURCE: process.env.NEXT_PUBLIC_IMAGE_SOURCE,
+    NEXT_PUBLIC_OG_SOURCE: process.env.NEXT_PUBLIC_OG_SOURCE,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
