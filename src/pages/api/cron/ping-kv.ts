a590db97484e50da60075ec09edd5060cd7548e0
Denys Churchyn <dodenisa@gmail.com>
2024-01-14 17:39:56 +0200

    Added cron job to ping vercel KV 2 times a month

---
+++ src/pages/api/cron/ping-kv.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { kv } from "@vercel/kv";
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (
+    !process.env.CRON_SECRET ||
+    request.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return response.status(401).json({ success: false });
+  }
+
+  kv.ping()
+    .then((pong) => {
+      response.status(200).json({ result: pong });
+    })
+    .catch((error) => {
+      console.error(error);
+      response.status(500).json({ success: false });
+    });
+}
