465bbb88d994ae3c4d5e148a81f17d494c842131
Denys Churchyn <dodenisa@gmail.com>
2024-01-21 17:29:20 +0200

    Added weekly statistics to TG

---
+++ src/pages/api/cron/notify-tg.ts
@@ -0,0 +1,109 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { kv } from "@vercel/kv";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (
+    !process.env.CRON_SECRET ||
+    request.headers.authorization !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return response.status(401).json({ success: false });
+  }
+
+  try {
+    const stats = await collectStats();
+    const notifyResponse = await notify(stats);
+    response.status(200).json({ success: true, message: notifyResponse });
+  } catch (error) {
+    response.status(500).json({ success: false, message: error });
+  }
+}
+
+type ImageStats = Record<string, number>;
+type SearchStats = Record<string, number>;
+
+async function collectStats() {
+  // Retrieve stats
+  const copyStats = await kv.hgetall<ImageStats>("copy__images");
+  const viewStats = await kv.hgetall<ImageStats>("view__images");
+  const searchStats = await kv.hgetall<SearchStats>("search__queries");
+
+  // Format stats
+  let statsString = "📊 *OrlyBooks Statistics* 📊\n\n\n";
+  statsString += formatTopStats("📎 *Image Copy Stats:*", copyStats);
+  statsString += "\n\n" + formatTopStats("🌅 *Image View Stats:*", viewStats);
+  statsString +=
+    "\n\n" + formatSearchStats("🔎 *User Search Queries:*", searchStats);
+
+  return statsString;
+}
+
+function formatTopStats(title: string, stats: ImageStats | null) {
+  let statsString = `${title}\n`;
+  if (stats) {
+    // Sort and get top 5
+    const sortedStats = Object.entries(stats)
+      .filter(([_, count]) => count > 1)
+      .sort(([_, a], [__, b]) => b - a)
+      .slice(0, 7);
+
+    for (const [name, count] of sortedStats) {
+      statsString += `_${name}_:  ${count}\n`;
+    }
+
+    // Total counts
+    const totalCounts = Object.values(stats).reduce(
+      (acc, count) => acc + Number(count),
+      0
+    );
+    const distinctCounts = Object.keys(stats).length;
+
+    statsString += `\n*Total:*  ${totalCounts}\n`;
+    statsString += `*Distinct:*  ${distinctCounts}\n`;
+  }
+  return statsString;
+}
+
+function formatSearchStats(title: string, stats: SearchStats | null) {
+  let statsString = `${title}\n`;
+  if (stats) {
+    // Aggregate searches and sort
+    const searchAggregates: Record<string, number> = {};
+    for (const query of Object.keys(stats)) {
+      const lowerQuery = query.toLowerCase();
+      searchAggregates[lowerQuery] = stats[query] ?? 0;
+    }
+
+    const sortedSearches = Object.entries(searchAggregates)
+      .sort(([_, a], [__, b]) => b - a)
+      .slice(0, 14);
+
+    statsString += "`";
+    for (const [query, count] of sortedSearches) {
+      statsString += `${query} (${count})\n`;
+    }
+    statsString += "`";
+  }
+  return statsString;
+}
+
+async function notify(data: string) {
+  const API_URL = `https://api.telegram.org/bot${process.env.TG_BOT_TOKEN}/sendMessage`;
+  const body = {
+    chat_id: process.env.TG_CHAT_ID,
+    text: data,
+    parse_mode: "Markdown",
+  };
+
+  const botResponse = await fetch(API_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  return JSON.stringify(await botResponse.json());
+}
