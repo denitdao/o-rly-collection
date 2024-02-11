eb6a7ace49e57c33ca078f8f1acba60c4951f8dc
Denys Churchyn <dodenisa@gmail.com>
2024-02-11 14:39:12 +0200

    Issue fix with click bypassing to lower elements

--- src/components/SortSelect.tsx
+++ src/components/SortSelect.tsx
@@ -1,4 +1,4 @@
-import { type SortMode } from "~/hooks/useBookSearch";
+import {type SortMode} from "~/hooks/useBookSearch";
 import {
   Select,
   SelectContent,
@@ -10,18 +10,28 @@ import {
 } from "~/components/ui/select";
 
 const SortSelect = ({
-  value,
-  onChange,
-}: {
+                      value,
+                      onChange,
+                    }: {
   value: SortMode;
   onChange: (value: SortMode) => void;
 }) => {
   return (
     <Select value={value} onValueChange={(v) => onChange(v as SortMode)}>
       <SelectTrigger className="w-[160px] font-mono">
-        <SelectValue placeholder="Select a sorting mode" />
+        <SelectValue placeholder="Select a sorting mode"/>
       </SelectTrigger>
-      <SelectContent className="font-mono">
+      <SelectContent
+        className="font-mono"
+        // Fix for mobile tap, triggering onClick for element behind the select
+        // https://github.com/shadcn-ui/ui/issues/486
+        ref={(ref) => {
+          if (!ref) return;
+          ref.ontouchstart = (e) => {
+            e.preventDefault();
+          };
+        }}
+      >
         <SelectGroup>
           <SelectLabel>Sorting Modes</SelectLabel>
           <SelectItem value="default">Default</SelectItem>
