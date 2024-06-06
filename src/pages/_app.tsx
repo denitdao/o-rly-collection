b83186f356d4d2ce02fba6a3aba5ba8cf3c262aa
Denys Churchyn <dodenisa@gmail.com>
2024-06-06 21:45:16 +0300

    Updated dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { AxiomWebVitals } from "next-axiom";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleAnalytics } from "~/components/meta/GoogleAnalytics";
@@ -23,7 +22,6 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       </PostHogAnalyticsProvider>
       <Analytics />
       <SpeedInsights />
-      <AxiomWebVitals />
       <GoogleAnalytics gaId="G-VB63CNRL7H" />
       <Toaster richColors closeButton theme="light" />
     </div>
