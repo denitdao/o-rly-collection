56b1bc85bb6de9f73100ff18e42fabdb400daa23
Denys Churchyn <dodenisa@gmail.com>
2024-08-11 15:01:18 +0300

    update dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleAnalytics } from "~/components/meta/GoogleAnalytics";
 import { Toaster } from "~/components/ui/sonner";
@@ -20,7 +19,6 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <PostHogAnalyticsProvider>
         <Component {...pageProps} />
       </PostHogAnalyticsProvider>
-      <Analytics />
       <SpeedInsights />
       <GoogleAnalytics gaId="G-VB63CNRL7H" />
       <Toaster richColors closeButton theme="light" />
