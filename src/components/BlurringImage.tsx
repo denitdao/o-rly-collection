234d49f9b38e8b6faccbdc2304706b24abbe3ac7
Denys Churchyn <dodenisa@gmail.com>
2024-09-20 23:11:30 +0300

    replaced tile with plain img (requires lazy loading optimization)

--- src/components/BlurringImage.tsx
+++ src/components/BlurringImage.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { AspectRatio } from "~/components/ui/aspect-ratio";
 
 const BlurringImage = ({
@@ -15,12 +14,12 @@ const BlurringImage = ({
       ratio={3 / 4.1}
       className="w-full overflow-hidden rounded-lg bg-gray-200 hover:cursor-pointer"
     >
-      <Image
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         alt={alt}
         src={imageUrl}
-        fill
         onClick={onClick}
-        className="scale-100 rounded-lg object-cover blur-0 grayscale-0 duration-500 ease-in-out group-hover:scale-95 group-hover:duration-200"
+        className="h-full w-full scale-100 rounded-lg object-cover blur-0 grayscale-0 duration-500 ease-in-out group-hover:scale-95 group-hover:duration-200"
       />
     </AspectRatio>
   );
