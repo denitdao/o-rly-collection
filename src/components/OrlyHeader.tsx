3c7135bac923f524959c73ff45142c0bb84fa549
Alexander <sashakryzh@gmail.com>
2024-11-01 10:53:54 +0100

    fix: updated button position and h1 text alignment

--- src/components/OrlyHeader.tsx
+++ src/components/OrlyHeader.tsx
@@ -3,11 +3,8 @@ import { Button } from "~/components/ui/button";
 
 const OrlyHeader = () => {
   return (
-    <header className="px-4 pb-16 pt-4">
-      <Link
-        href="https://make.orlybooks.com/"
-        className="mb-8 flex justify-end"
-      >
+    <header className="mx-auto flex w-full max-w-screen-2xl flex-col items-center px-4 pb-16 pt-4">
+      <Link href="https://make.orlybooks.com/" className="mb-8 flex self-end">
         <div className="group relative">
           <span
             className="absolute inset-0 animate-rainbow rounded-md bg-[linear-gradient(90deg,theme('colors.red.300'),theme('colors.purple.300'),theme('colors.blue.300'),theme('colors.cyan.300'),theme('colors.lime.300'),theme('colors.orange.300'))] bg-[length:200%] blur-[5px] group-hover:blur-[2px]"
@@ -21,21 +18,19 @@ const OrlyHeader = () => {
           </Button>
         </div>
       </Link>
-      <div className="mx-auto flex w-full max-w-screen-2xl flex-col items-center">
-        <h1 className="mb-5 text-4xl font-extrabold text-black">
-          Search O&apos;RLY Covers
-        </h1>
-        <h2 className="text-center font-mono text-gray-600">
-          Strengthen your{" "}
-          <Link
-            href="/"
-            className="underline decoration-blue-400 decoration-2 underline-offset-2"
-          >
-            arguments
-          </Link>{" "}
-          with compelling programming book covers
-        </h2>
-      </div>
+      <h1 className="mb-5 text-center text-4xl font-extrabold text-black">
+        Search O&apos;RLY Covers
+      </h1>
+      <h2 className="text-center font-mono text-gray-600">
+        Strengthen your{" "}
+        <Link
+          href="/"
+          className="underline decoration-blue-400 decoration-2 underline-offset-2"
+        >
+          arguments
+        </Link>{" "}
+        with compelling programming book covers
+      </h2>
     </header>
   );
 };
